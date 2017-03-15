11d7229083c92202dbe7bd6c208ae5f99fc9f07f
Hans Becker <StareInTheAir@users.noreply.github.com>
2017-03-15 12:14:22 +0100

    Create windows10-uninstall-apps.ps1
    Added PowerShell script for removing default Windows 10 apps

---
+++ windows10-uninstall-apps.ps1
@@ -0,0 +1,22 @@
+Get-AppxPackage *alarms*|Remove-AppxPackage
+Get-AppxPackage *weather*|Remove-AppxPackage
+Get-AppxPackage *photos*|Remove-AppxPackage
+Get-AppxPackage *maps*|Remove-AppxPackage
+Get-AppxPackage *recorder*|Remove-AppxPackage # Voice Recorder
+Get-AppxPackage *stickynotes*|Remove-AppxPackage
+Get-AppxPackage *video*|Remove-AppxPackage # Movies & TV
+Get-AppxPackage *people*|Remove-AppxPackage # Contacts 
+Get-AppxPackage *music*|Remove-AppxPackage
+Get-AppxPackage *camera*|Remove-AppxPackage
+Get-AppxPackage *onenote*|Remove-AppxPackage
+Get-AppxPackage *xboxapp*|Remove-AppxPackage
+
+Get-AppxPackage *3dbuilder*|Remove-AppxPackage
+Get-AppxPackage *appinstaller*|Remove-AppxPackage
+Get-AppxPackage *feedbackhub*|Remove-AppxPackage
+Get-AppxPackage *officehub*|Remove-AppxPackage # Get Office
+Get-AppxPackage *getstarted*|Remove-AppxPackage
+Get-AppxPackage *communications*|Remove-AppxPackage # mail and calendar
+Get-AppxPackage *solitaire*|Remove-AppxPackage 
+Get-AppxPackage *skype*|Remove-AppxPackage # skype app
+Get-AppxPackage *oneconnect*|Remove-AppxPackage # Paid WiFi & Cellular
